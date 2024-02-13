1aa9874b6107ef21f8e369c0f5b60f7c9edaf891
kimgura <oli999@naver.com>
2024-02-13 09:50:02 +0900

    react SPA 기능 테스트중...

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 
 import { Component } from 'react';
 import './App.css'
+import { Link, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Study from './pages/Study';
+import Play from './pages/Play';
+import NotFound from './pages/NotFound';
+import Todo from './pages/Todo';
 
 //클래스형 component
 class App extends Component{
-
+  
   //render() 함수에서 리턴하는 jsx 로 화면 구성이 된다.
   render(){
 
     return (
       <div className="container">
-        <h1>인덱스 페이지 입니다</h1>
-
+        <h1>React Router 를 이용한 SPA 테스트</h1>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/study">Study</Link></li>
+          <li><Link to="/play">Play</Link></li>
+          <li><Link to="/todos">Todos</Link></li>
+        </ul>
+        <Routes>
+          <Route path="/" Component={Home}/>
+          <Route path="/study" Component={Study}/>
+          <Route path="/play" Component={Play}/>
+          <Route path="/todos" Component={Todo}/>
+          <Route path="/*" Component={NotFound}/>
+        </Routes>
       </div>
     )
   }
---
+++ src/pages/Play.js
@@ -0,0 +1,17 @@
+// src/pages/Play.js
+
+const { Component } = require("react");
+
+class Play extends Component{
+    render(){
+        
+        return (
+            <>
+                <h3>Play 페이지 입니다</h3>
+                <p>열심히 놀아요</p>     
+            </>
+        )
+    }
+}
+
+export default Play
