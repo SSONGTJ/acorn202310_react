4c3aafeda6479b9fddd7bee91ed7d4f22c562501
kimgura <oli999@naver.com>
2024-03-05 11:54:46 +0900

    Step16 에 Gallery 자세히보기 Prev, Next 버튼 구현함

--- src/pages/GalleryDetail.js
+++ src/pages/GalleryDetail.js
@@ -2,34 +2,49 @@
 
 import axios from "axios"
 import { useEffect, useState } from "react"
-import { Card } from "react-bootstrap"
-import { useParams } from "react-router-dom"
+import { Button, Card, Pagination } from "react-bootstrap"
+import { useSelector } from "react-redux"
+import { Link, useParams } from "react-router-dom"
 
 export default function GalleryDetail(){
     // "/gallery/:num" 에서 num 에 해당하는 경로 파라키터 값 읽어오기
     const {num}=useParams()
     //gallery 하나의 정보를 상태값으로 관리 
-    const [state, setState]=useState({})
+    const [state, setState]=useState(null)
+
+    //로그인된 사용자명이 store 에 있는지 읽어와 본다. 
+    const userName=useSelector(state=>state.userName)
+
     useEffect(()=>{
         axios.get("/gallery/"+num)
         .then(res=>{
+            console.log(res.data)
             setState(res.data)
         })
         .catch(error=>{
             console.log(error)
         })
-    }, [])
+    }, [num]) //경로 파라미터가 변경될때 서버로 부터 데이터를 다시 받아오도록 한다. 
     return (
         <>
             <h3>Gallery 자세히 보기 페이지</h3>
-            <Card>
-                <Card.Img variant="top" src={`/upload/images/${state.saveFileName}`}/>
-                <Card.Body>
-                    <Card.Text>{state.caption}</Card.Text>
-                    <Card.Text>writer : <strong>{state.writer}</strong></Card.Text>
-                    <Card.Text>{state.regdate}</Card.Text>
-                </Card.Body>
-            </Card>
+            { state && 
+                <>
+                    <Pagination>
+                        <Pagination.Item disabled={state.prevNum === 0} as={Link} to={"/gallery/"+state.prevNum}>&larr; Prev</Pagination.Item>
+                        <Pagination.Item disabled={state.nextNum ===} as={Link} to={"/gallery/"+state.nextNum}>Next &rarr;</Pagination.Item>
+                    </Pagination>
+                    <Card>
+                        <Card.Img variant="top" src={`/upload/images/${state.saveFileName}`}/>
+                        <Card.Body>
+                            <Card.Text>{state.caption}</Card.Text>
+                            <Card.Text>writer : <strong>{state.writer}</strong></Card.Text>
+                            <Card.Text>{state.regdate}</Card.Text>
+                            { userName === state.writer && <Button variant="danger">삭제</Button> }
+                        </Card.Body>
+                    </Card>
+                </>
+            }
         </>
     )
 }
