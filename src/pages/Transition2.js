c6f81026e1d73b9326d925de52ac86a17b7136d8
kimgura <oli999@naver.com>
2024-03-08 10:06:30 +0900

    Step16 에  router 정보를 다른 파일로 분리함

--- src/pages/Transition2.js
+++ src/pages/Transition2.js
@@ -11,6 +11,13 @@ import { CSSTransition } from 'react-transition-group'
 export default function Transition2(){
 
     const [boxShow, setBoxShow]=useState(true)
+    /*
+        CSSTransition 컴포넌트에 classNames 속성에 넣어줄 object 
+
+        enter 혹은 exit 될때 우리가 정의한 클래스 속성값이 자동으로 추가 되도록 
+
+        하는 object 이다 
+    */
     const tranClass={
         enter:"animate__animated",
         enterActive:"animate__rotateIn",
---
+++ src/router/router.js
@@ -0,0 +1,42 @@
+// src/router/router.js
+
+import { createBrowserRouter } from "react-router-dom"
+import Book from "../pages/Book"
+import EditorComponent from "../pages/EditorComponent"
+import Gallery from "../pages/Gallery"
+import GalleryDetail from "../pages/GalleryDetail"
+import Home from "../pages/Home"
+import Member from "../pages/Member"
+import MemberForm from "../pages/MemberForm"
+import MemberUpdateForm from "../pages/MemberUpdateForm"
+import Transition from "../pages/Transition"
+import Transition2 from "../pages/Transition2"
+import App from "../App"
+
+//라우트 정보를 배열에 저장하기
+const routes=[
+    {path:"/", element:<Home/>},
+    {path:"/members", element:<Member/>},
+    {path:"/members/new", element:<MemberForm/>},
+    {path:"/members/:num/edit", element:<MemberUpdateForm/>},
+    {path:"/gallery", element:<Gallery/>},
+    {path:"/gallery/:num", element:<GalleryDetail/>},
+    {path:"/editor", element:<EditorComponent/>},
+    {path:"/book", element:<Book/>},
+    {path:"/transition", element:<Transition/>},
+    {path:"/transition2", element:<Transition2/>}
+  ]
+//BrowserRouter 를 custom 으로 만들기
+const router = createBrowserRouter([{
+    path:"/",  //현재 경로가 최상위 경로 일때
+    element:<App/>, //App Component 를 사용하겠다 
+    children: routes.map((route)=>{  //자식 컴포넌트 정보 등록 
+      return {
+        index: route.path === "/", //자식의 path 가 "/" 면 index 페이지 역활을 하게 하기 
+        path: route.path === "/" ? undefined : route.path, // path 에 "/" 두개가 표시 되지 안토록  
+        element: route.element //어떤 컴포넌트를 활성화 할것인지 
+      }
+    })
+}])
+
+export default router
