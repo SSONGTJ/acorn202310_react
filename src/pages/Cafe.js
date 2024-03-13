81839f3fbdb0cbfe5345e05e060392a73f912cae
kimgura <oli999@naver.com>
2024-03-13 12:04:01 +0900

    Step16 에 Cafe 검색 기능 구현함

--- src/pages/Cafe.js
+++ src/pages/Cafe.js
@@ -11,7 +11,17 @@ export default function Cafe(){
         const [pageInfo, setPageInfo]=useState({
             list:[]
         })
-    
+        //검색 조건과 키워드를 상태값으로 관리
+        const [searchState, setSearchState]=useState({
+            condition:"",
+            keyword:""
+        })
+
+        //searchState 값을 테스트로 확인해 보기 
+        // useEffect(()=>{
+        //     console.log(searchState)
+        // }, [searchState])
+
         //페이징 UI 를 만들때 사용할 배열
         const [pageArray, setPageArray]=useState([])
         // "/cafes?pageNum=x" 에서 pageNum 을 추출하기 위한 Hook   
@@ -30,7 +40,9 @@ export default function Cafe(){
     
         //겔러리 목록 데이터 읽어오는 함수
         const refresh = (pageNum)=>{
-            axios.get("/cafes?pageNum="+pageNum)
+            //검색 기능과 관련된 query 문자열 읽어오기
+            const query=new URLSearchParams(searchState).toString()
+            axios.get("/cafes?pageNum="+pageNum+"&"+query)
             .then(res=>{
                 console.log(res.data)
                 //카페 글목록 출력
@@ -43,7 +55,11 @@ export default function Cafe(){
                 console.log(error)
             })
         }
-    
+        /*
+            useEffect() 안에 전달한 함수는
+            1. Cafe 컴포넌트가 활성화 되는 시점에 1번 호출된다.
+            2. params 가 변경이 될때마다 호출된다 
+        */
         useEffect(()=>{
             //query 파라미터 값을 읽어와 본다
             let pageNum=params.get("pageNum")
@@ -52,7 +68,23 @@ export default function Cafe(){
             //해당 페이지의 내용을 원격지 서버로 부터 받아온다 
             refresh(pageNum)
         }, [params]) // params 가 변경되었을때도 다시 받아오도록 한다 
-      
+    
+        // 검색 select , input 요소에 변화가 생겼을때 호출되는 함수 
+        const handleSearchChange = (e) => {
+            setSearchState({
+                ...searchState,
+                [e.target.name]:e.target.value
+            })
+            //검색 버튼 누르지 않고 바로 검색되기를 원하면 여기서 updateNavigate() 함수를 호출해 준다.
+            //updateNavigate()
+        }
+
+        // navigate() 함수를 호출해서 위치를 변경하는 함수
+        const updateNavigate = (pageNum=1)=>{
+            //검색조건에 맞는 query 문자열을 얻어내기 
+            const query = new URLSearchParams(searchState).toString()
+            navigate(`/cafes?pageNum=${pageNum}&${query}`)
+        }
 
     return (
         <>
@@ -71,7 +103,7 @@ export default function Cafe(){
                 <tbody>
                     {
                         pageInfo.list.map(item=>(
-                            <tr>
+                            <tr key={item.num}>
                                 <td>{item.num}</td>
                                 <td>
                                     <Link to={`/cafes/${item.num}`}>{item.title}</Link>
@@ -85,19 +117,35 @@ export default function Cafe(){
                 </tbody>
             </Table>
             <Pagination className="mt-3">
-              <Pagination.Item onClick={()=>{
-                //setParmas() 함수를 이용해서 query 파라미터를 변경한다(history 에 누적이 됨)
-                setParams({pageNum:pageInfo.startPageNum-1})
-              }} disabled={pageInfo.startPageNum === 1}>&laquo;</Pagination.Item> 
+              <Pagination.Item onClick={()=> updateNavigate(pageInfo.startPageNum-1)} disabled={pageInfo.startPageNum === 1}>&laquo;</Pagination.Item> 
               {
-                pageArray.map(item=>(<Pagination.Item onClick={()=>{
-                  setParams({pageNum:item})
-                }}  key={item} active={pageInfo.pageNum === item}>{item}</Pagination.Item>))
+                pageArray.map(item=>(<Pagination.Item onClick={()=> updateNavigate(item)}  key={item} active={pageInfo.pageNum === item}>{item}</Pagination.Item>))
               }
-              <Pagination.Item onClick={()=>{
-                setParams({pageNum:pageInfo.endPageNum+1})
-              }} disabled={pageInfo.endPageNum >= pageInfo.totalPageCount }>&raquo;</Pagination.Item> 
-            </Pagination>  
+              <Pagination.Item onClick={()=> updateNavigate(pageInfo.endPageNum+1)} disabled={pageInfo.endPageNum >= pageInfo.totalPageCount }>&raquo;</Pagination.Item> 
+            </Pagination> 
+
+            <label htmlFor="search">검색조건</label>
+            <select id="search" name="condition" value={searchState.condition} 
+                onChange={handleSearchChange}>
+              <option value="">선택</option>
+              <option value="title_content">제목 + 내용</option>
+              <option value="title">제목</option>
+              <option value="writer">작성자</option>
+            </select> 
+            <input type="text" placeholder="검색어..." name="keyword" value={searchState.keyword} 
+                onChange={handleSearchChange}/>
+            <button onClick={()=>updateNavigate()}>검색</button> 
+            <button onClick={()=>{
+                setSearchState({condition:"", keyword:""})
+                updateNavigate()
+            }}>Reset</button>
+            <p> <strong>{pageInfo.totalRow}</strong> 개의 글이 있습니다</p> 
         </>
     )
 }
+
+
+
+
+
+
